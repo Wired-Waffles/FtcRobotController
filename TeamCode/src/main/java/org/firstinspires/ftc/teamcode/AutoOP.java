19fa22fdc56b5dc54079a6c581f5c32e0f3cb4c4
optimusgladius <optimusgladius@gmail.com>
2025-04-07 16:22:21 +0100

    testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOP.java
@@ -1,5 +1,5 @@
 package org.firstinspires.ftc.teamcode;
-//testing
+//testing 123
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
