ccde4e435bd0885507d3973f1d85c143d7d09194
Techon ;- <148909669+TechonVS@users.noreply.github.com>
2025-11-22 11:03:58 +0000

    new april tag data reader

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.Range;
 
-import org.firstinspires.ftc.teamcode.mechanisms.AprilTagVision;
+import org.firstinspires.ftc.teamcode.mechanisms.AprilTagRead;
 import org.firstinspires.ftc.teamcode.mechanisms.DriveChainTank;
 import org.firstinspires.ftc.teamcode.mechanisms.RubberBandIntake;
 import org.firstinspires.ftc.vision.VisionPortal;
@@ -20,56 +20,31 @@ public class TeleOP extends LinearOpMode {
 
     //mech obj declare
     DriveChainTank driveChain = new DriveChainTank();
-    AprilTagVision aprilTagVision = new AprilTagVision();
     RubberBandIntake intake = new RubberBandIntake();
+    AprilTagRead aprilTagReader = new AprilTagRead();
     //everything bloody else
-    final double DESIRED_DISTANCE = 24.0; //this is how close the camera should get to the target (inches)
-
-    //vision constants from external.samples.RobotAutoDriveToAprilTagTank example op mode
-    //  Set the GAIN constants to control the relationship between the measured position error, and how much power is
-    //  applied to the drive motors to correct the error.
-    //  Drive = Error * Gain    Make these values smaller for smoother control, or larger for a more aggressive response.
-    final double SPEED_GAIN =   0.02 ;   //  Speed Control "Gain". e.g. Ramp up to 50% power at a 25 inch error.   (0.50 / 25.0)
-    final double TURN_GAIN  =   0.01 ;   //  Turn Control "Gain".  e.g. Ramp up to 25% power at a 25 degree error. (0.25 / 25.0)
-    final double MAX_AUTO_SPEED = 0.5;   //  Clip the approach speed to this max value (adjust for your robot)
-    final double MAX_AUTO_TURN  = 0.25;  //  Clip the turn speed to this max value (adjust for your robot)
-
-    private static final boolean USE_WEBCAM = true;  // Set true to use a webcam, or false for a phone camera
-    private static final int DESIRED_TAG_ID = -1;    // Choose the tag you want to approach or set to -1 for ANY tag.
-    private VisionPortal visionPortal;               // Used to manage the video source.
-    private AprilTagProcessor aprilTag;              // Used for managing the AprilTag detection process.
-    private AprilTagDetection desiredTag = null;     // Used to hold the data for a detected AprilTag
-
     double drivePower;
     double turn;
+
+    double driveMod = 1.5;
     @Override
     public void runOpMode() throws InterruptedException {
         boolean targetFound     = false;
         driveChain.init(hardwareMap);
-        aprilTagVision.init(hardwareMap);
         intake.init(hardwareMap);
+        aprilTagReader.init(hardwareMap, telemetry);
         waitForStart();
         while (opModeIsActive()) {
-
-            //auto aim via april tag
-            if (gamepad1.left_bumper && targetFound) {
-                //find heading and range error to help calc the robots movement
-                double  rangeError   = (desiredTag.ftcPose.range - DESIRED_DISTANCE);
-                double  headingError = desiredTag.ftcPose.bearing;
-
-                //use the speed and turn gain constants times the errors to calc robot movement.
-                // clip to max
-                drivePower = Range.clip(rangeError * SPEED_GAIN, -MAX_AUTO_SPEED, MAX_AUTO_SPEED);
-                turn  = Range.clip(headingError * TURN_GAIN, -MAX_AUTO_TURN, MAX_AUTO_TURN) ;
-
-                telemetry.addData("Auto","Drive %5.2f, Turn %5.2f", drivePower, turn);
-            } else {
-                //default single joystick tank drive
-                drivePower = -gamepad1.left_stick_y  / 1.5;  // Reduce drive rate to 50%.
-                turn  = gamepad1.right_stick_x / 2.0;  // Reduce turn rate to 25%.
-                telemetry.addData("Manual","Drive %5.2f, Turn %5.2f", drivePower, turn);
+            aprilTagReader.update();
+            AprilTagDetection id20 = aprilTagReader.getTagByID(20);
+            aprilTagReader.displayTelemetry(id20);
+            if (gamepad1.options) {
+                driveMod = 1;
             }
+            drivePower = -gamepad1.left_stick_y  / driveMod;
+            turn  = gamepad1.right_stick_x / 2.0;
 
+            telemetry.addData("Manual","Drive %5.2f, Turn %5.2f", drivePower, turn);
             driveChain.moveRobot(drivePower, turn);
             telemetry.update();
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/AprilTagRead.java
@@ -0,0 +1,71 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+
+import android.util.Size;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AprilTagRead {
+    private AprilTagProcessor aprilTagProcessor;
+    private VisionPortal vision;
+    private List<AprilTagDetection> detectedTags = new ArrayList<>();
+    private Telemetry telemetry;
+
+    public void init(HardwareMap hardwareMap, Telemetry telemetry) {
+        this.telemetry = telemetry;
+        aprilTagProcessor = new AprilTagProcessor.Builder()
+                .setDrawTagID(true)
+                .setDrawTagOutline(true)
+                .setDrawAxes(true)
+                .setDrawCubeProjection(true)
+                .setOutputUnits(DistanceUnit.CM, AngleUnit.DEGREES)
+                .build();
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+        builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
+        builder.setCameraResolution(new Size(640, 480));
+        builder.addProcessor(aprilTagProcessor);
+
+        vision = builder.build();
+    }
+    public void update(){
+        detectedTags = aprilTagProcessor.getDetections();
+
+    }
+
+    public List<AprilTagDetection> getDetectedTags() {
+        return detectedTags;
+    }
+
+    public void displayTelemetry(AprilTagDetection ID) {
+        if (ID == null) return;
+        if (ID.metadata != null) {
+            telemetry.addLine(String.format("\n==== (ID %d) %s", ID.id, ID.metadata.name));
+            telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", ID.ftcPose.x, ID.ftcPose.y, ID.ftcPose.z));
+            telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", ID.ftcPose.pitch, ID.ftcPose.roll, ID.ftcPose.yaw));
+            telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", ID.ftcPose.range, ID.ftcPose.bearing, ID.ftcPose.elevation));
+        } else {
+            telemetry.addLine(String.format("\n==== (ID %d) Unknown", ID.id));
+            telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", ID.center.x, ID.center.y));
+        }
+    }
+
+    public AprilTagDetection getTagByID(int ID){
+        for (AprilTagDetection detection : detectedTags) {
+            if (detection.id == ID) return detection;
+        }
+        return null;
+    }
+    public void kill() {
+        if (vision != null) vision.close();
+    }
+}
